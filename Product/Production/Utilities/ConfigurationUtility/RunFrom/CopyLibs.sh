5f430856805b40702c29f851bc6d3fcb12d8046b
leswestberg <leswestberg@fake.com>
2011-10-21 17:22:10 +0000

    Story:artf1748 Author:Westberg Description:Linux scripts to run the Configuration Utility.

---
+++ Product/Production/Utilities/ConfigurationUtility/RunFrom/CopyLibs.sh
@@ -0,0 +1,6 @@
+cd $NHINC_SOURCE_DIR/Product/Production/Utilities/ConfigurationUtility/RunFrom
+cp $NHINC_SOURCE_DIR/Product/Production/Utilities/ConfigurationUtility/dist/debug/ConfigurationUtility.jar .
+cp $NHINC_SOURCE_DIR/Product/Production/Common/AurionCoreLib/dist/debug/AurionCoreLib.jar .
+cp $NHINC_SOURCE_DIR/ThirdParty/log4j/commons-logging-1.1.1.jar .
+cp $NHINC_SOURCE_DIR/ThirdParty/log4j/log4j-1.2.15.jar .
+cp $NHINC_SOURCE_DIR/ThirdParty/SpringFramework/spring-2.5.6.SEC03.jar .
---
+++ Product/Production/Utilities/ConfigurationUtility/RunFrom/SwitchToJava.sh
@@ -0,0 +1,2 @@
+java -Dconfig.util.log.dir=$NHINC_SOURCE_DIR/Product/Production/Utilities/ConfigurationUtility/RunFrom -Dnhinc.properties.dir=$AS_HOME/domains/domain1/config/nhin -jar ConfigurationUtility.jar java
+
