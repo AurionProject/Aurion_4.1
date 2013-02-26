aad42c5669284207899374c8b2993ef7f8dd38a3
neilwebb <neilkwebb@hotmail.com>
2013-02-26 18:44:07 +0000

    Author: Neil Webb, Comments: Updated shell script build path and added ant targets to match Windows build.

--- Build/hudsonbuild.sh
+++ Build/hudsonbuild.sh
@@ -1,9 +1,9 @@
 pwd
 set -x
-cd Current
+cd 4.1
 . Build/sethudsonenv.sh
 cd "$NHINC_SOURCE_DIR/Product"
 env | sort
 type ant
-sh "$ANT_HOME/bin/ant" clean package.create
+sh "$ANT_HOME/bin/ant" clean package.create unittest.run copy.deployable.artifacts publish.create
 
